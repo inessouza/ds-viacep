a9abd4253cd9bc80a4adb384ff2acb488c4ee5a3
Ines Souza <inessouzasp@gmail.com>
2023-02-20 21:35:47 -0300

    Projeto DS ViaCEP

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -1,7 +1,11 @@
-
-
+import Address from '../models/address.js'
+import * as addressService from '../services/address-service.js'
+import * as listController from './list-controller.js'
 
 function State () {
+
+    this.address = new Address()
+
     this.btnSave = null
     this.btnClear = null
 
@@ -28,5 +32,68 @@ export function init() {
     state.errorCep = document.querySelector('[data-error="cep"]')
     state.errorNumber = document.querySelector('[data-error="number"]')
 
-    console.log(state)
+    state.inputNumber.addEventListener('change', handleInputNumberChange)
+    state.inputNumber.addEventListener('keyup', handleInputNumberKeyup)
+    state.btnClear.addEventListener('click', handleBtnClearClick)
+    state.btnSave.addEventListener('click', handleBtnSaveClick)
+    state.inputCep.addEventListener('change', handleInputCepChange)
 } 
+
+function handleInputNumberKeyup(event) {
+    state.address.number = event.target.value
+}
+
+async function handleInputCepChange(event) {
+    const cep = event.target.value
+
+    try {
+    const address = await addressService.findByCep(cep)
+
+    state.inputStreet.value = address.street
+    state.inputCity.value = address.city
+    state.address = address
+
+    setFormError("cep", "")
+    state.inputNumber.focus()
+    }
+    catch (e) {
+        state.inputStreet.value = ""
+        state.inputCity.value = ""
+        setFormError("cep", "Informe um CEP válido.")
+    }
+}
+
+async function handleBtnSaveClick(event) {
+    event.preventDefault()
+    listController.addCard(state.address)
+}
+
+function handleInputNumberChange(event) {
+    if (event.target.value == "") {
+        setFormError("number", "Campo Requerido")
+    } else {
+        setFormError("number", "")
+    }
+}
+
+function handleBtnClearClick(event) {
+    event.preventDefault()
+    clearForm()
+}
+
+function clearForm() {
+    state.inputCep.value = ""
+    state.inputCity.value = ""
+    state.inputNumber.value = ""
+    state.inputStreet.value = ""
+
+    setFormError("cep", "")
+    setFormError("number", "")
+
+    state.inputCep.focus()
+}
+
+function setFormError(key, value) {
+    const element = document.querySelector(`[data-error="${key}"]`)
+    element.innerHTML = value
+}
